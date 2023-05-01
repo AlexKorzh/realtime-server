276da3d0e6d0f08989a542ff6c4a8528ea43a30f
alexkorzh <korzhlesha@gmail.com>
2023-05-01 23:06:10 +0200

    updateRadius util added

--- public/utils.js
+++ public/utils.js
@@ -16,6 +16,13 @@ function rgbFloatToHex(r, g, b) {
     element.style.top = `${nodeProps.y}px`;
   };
 
+  const updateRadius = (element, nodeProps) => {
+    element.style.borderTopLeftRadius = `${nodeProps.borderTopLeftRadius}px`;
+    element.style.borderTopRightRadius = `${nodeProps.borderTopRightRadius}px`;
+    element.style.borderBottomLeftRadius = `${nodeProps.borderBottomLeftRadius}px`;
+    element.style.borderBottomRightRadius = `${nodeProps.borderBottomRightRadius}px`;
+  };
+
   const updateColor = (element, color) => {
     element.style.backgroundColor = rgbFloatToHex(color.r, color.g, color.b);
   };
@@ -25,6 +32,7 @@ function rgbFloatToHex(r, g, b) {
 
     updateDimensions(node, nodeProps);
     updatePosition(node, nodeProps);
+    updateRadius(node, nodeProps);
     updateColor(node, nodeProps.color);
 
     node.className = 'shape';
@@ -37,4 +45,14 @@ function rgbFloatToHex(r, g, b) {
     const node = document.getElementById(nodeId);
 
     node.parentNode.removeChild(node);
+  }
+
+  const updateNode = (node) => {
+    const shape = document.getElementById(node.id);
+    const { color } = node;
+    
+    updateDimensions(shape, node);
+    updatePosition(shape, node);
+    updateRadius(shape, node);
+    updateColor(shape, color);
   }
